cd19070928217c9b0276d36fbf733116c0d437f5
tallgeeze <57326846+tallgeeze@users.noreply.github.com>
2021-06-05 22:56:33 +0200

    Add files via upload

---
+++ Ex05_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex05/p1/Rectangle.java
@@ -0,0 +1,189 @@
+package de.unistuttgart.vis.dsass2021.ex05.p1;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+/**
+ * This class represents a quadrilateral, where the angles are 90 degrees.
+ */
+public class Rectangle {
+
+    // Upper left corner of the rectangle
+    private final float x;
+    private final float y;
+    private final float width;
+    private final float height;
+
+    /**
+     * Generate a new rectangle specified with the upper left corner, width and
+     * height. Note that y values the coordinate systems increase from top to down
+     * as usual in computer graphics.
+     * 
+     * @param leftXValue  left position of the rectangle
+     * @param upperYValue upper position of the rectangle
+     * @param width       the width of the rectangle. The width must be greater than
+     *                    0.
+     * @param heigth      the height of the rectangle The height must be greater
+     *                    than 0.
+     * @throws IllegalArgumentException if width or height are 0 or less.
+     */
+    public Rectangle(final float leftXValue, final float upperYValue, final float width, final float height) {
+        if (width <= 0 || height <= 0) {
+            throw new IllegalArgumentException("width or height are 0 or less");
+        }
+        this.x = leftXValue;
+        this.width = width;
+        this.y = upperYValue;
+        this.height = height;
+    }
+
+    public float getX() {
+        return x;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    /**
+     * Checks whether a given point is inside this rectangle.
+     * 
+     * The point is inside the rectangle if all it is on the corners, edges or
+     * inside the rectangle.
+     * 
+     * 
+     * @param point the point to check
+     * @return true if the point is inside the rectangle false if not
+     * @throws IllegalArgumentException if the point is null
+     */
+    public boolean containsPoint(final Point point) {
+        if (point == null) {
+            throw new IllegalArgumentException("point is null");
+        }
+        final float rightXCoordinate = this.x + this.width;
+        final float lowerYCoordinate = this.y + this.height;
+        if (point.getXValue() >= this.x && point.getXValue() <= rightXCoordinate && point.getYValue() >= this.y
+                && point.getYValue() <= lowerYCoordinate) {
+            return true;
+        }
+        return false;      
+    }
+
+    /**
+     * Checks whether a given rectangle intersects this rectangle.
+     * 
+     * The given rectangle intersects this rectangle if it is (1) inside this
+     * rectangle, (2) both rectangles have an overlapping part inside, or (3) both
+     * rectangles overlap on the edge.
+     * 
+     * @param rectangle the rectangle to check
+     * @return true if the rectangle intersects this rectangle else false
+     * @throws IllegalArgumentException if the rectangle is null
+     */
+    public boolean intersects(final Rectangle rectangle) {
+        if (rectangle == null) {
+            throw new IllegalArgumentException("rectangle is null");
+        }
+
+        final boolean intersectionOnXAxis = intervalIntersection(this.x, this.x + this.width, rectangle.x,
+                rectangle.x + rectangle.width);
+        final boolean intersectionOnYAxis = intervalIntersection(this.y, this.y + this.height, rectangle.y,
+                rectangle.y + rectangle.height);
+        if (intersectionOnXAxis && intersectionOnYAxis) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * @param intervalLeftThisRectanlge   First interval left
+     * @param intervalRightThisRectangle  First interval right
+     * @param intervalLeftOtherRectangle  Second interval left
+     * @param intervalRightOtherRectangle Second interval right
+     * @return true if the two intervals overlap
+     */
+    private boolean intervalIntersection(float intervalLeftThisRectanlge, float intervalRightThisRectangle,
+            float intervalLeftOtherRectangle, float intervalRightOtherRectangle) {
+        if ((intervalLeftOtherRectangle >= intervalLeftThisRectanlge)
+                && (intervalLeftOtherRectangle <= intervalRightThisRectangle))
+            return true;
+        if ((intervalLeftThisRectanlge >= intervalLeftOtherRectangle)
+                && (intervalLeftThisRectanlge <= intervalRightOtherRectangle))
+            return true;
+        return false;
+    }
+
+    /**
+     * Compute the bounding rectangle for a collection of rectangles.
+     * 
+     * @param rectangles the collection of rectangles which should be included in
+     *                   the bounding rectangle
+     * @return the bounding rectangle
+     * @throws IllegalArgumentException if rectangles or one of its elements is null
+     *                                  or if rectangles is empty
+     */
+    public static Rectangle getBoundingBox(final Collection<Rectangle> rectangles) {
+        if (rectangles == null || rectangles.contains(null) || rectangles.isEmpty()) {
+            throw new IllegalArgumentException("rectangles or one of its elements is null or rectangles is empty");
+        }
+        float minX = Float.MAX_VALUE;
+        float maxX = -Float.MAX_VALUE;
+        float minY = Float.MAX_VALUE;
+        float maxY = -Float.MAX_VALUE;
+        for (Rectangle rectangle : rectangles) {
+            if (rectangle.x < minX) {
+                minX = rectangle.x;
+            }
+            if (rectangle.x + rectangle.width > maxX) {
+                maxX = rectangle.x + rectangle.width;
+            }
+            if (rectangle.y < minY) {
+                minY = rectangle.y;
+            }
+            if (rectangle.y + rectangle.height > maxY) {
+                maxY = rectangle.y + rectangle.height;
+            }
+        }
+        return new Rectangle(minX, minY, maxX - minX, maxY - minY);
+    }
+
+    /**
+     * Computes a new rectangle based on this rectangle where the size is decreased
+     * so none of the given points are included in the new rectangle. The upper left
+     * corner of this rectangle has to stay the same.
+     * 
+     * @param points - A <code>Collection</code> of Points for example an ArrayList
+     * @return the new rectangle with decreased size
+     * @throws IllegalArgumentException if the collection or one the points in
+     *                                  contains are null
+     * 
+     */
+    public Rectangle excludePoints(final Collection<Point> points) {
+        // TODO Insert code for assignment 5.1.a
+    	// firstly, we gonna find the point, which has Xmin and Ymin, and get its value.
+    	// we compose an Iterator through poitns object
+    	Iterator<Point> point_iterator = points.iterator();
+    	float X_min = this.x + this.height , Y_min = this.y + this.width;
+    	while( point_iterator.hasNext() ) {
+    		Point current_point = point_iterator.next();
+    		if( current_point.getXValue() < X_min | current_point.getYValue() < Y_min ) {
+    		X_min = current_point.getXValue();
+    		Y_min = current_point.getYValue();
+    		}
+    	}
+    	//so far we stack the most left and up point X,Y value as X_min, Y_min
+    	//now we create a new bounding box accordint to it, but with distance 1 from edges.
+    	Rectangle new_Rectangle = new Rectangle(this.x, this.y, X_min-this.x-1, Y_min-this.y-1   );
+    	return new_Rectangle;
+    }
+
+}
---
+++ Ex05_ChenDengYao/src/main/java/de/unistuttgart/vis/dsass2021/ex05/p1/SimpleQuadTree.java
@@ -0,0 +1,192 @@
+package de.unistuttgart.vis.dsass2021.ex05.p1;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * The class SimpleQuad
+ */
+
+public class SimpleQuadTree<T extends QuadTreeElement> extends QuadTree<T> {
+
+    /**
+     * a constructor of a SimpleQuadTree with 2 parameters. A bounding box will be
+     * computed by the algorithm.
+     *
+     * @param elements,          a list of elements to be saved in the quadtree.
+     * @param maxElementsInLeaf, the maximum limit of elements, in which a leaf node
+     *                           can save.
+     * @throws IllegalArgumentException, if parameter is missing, or element ==
+     *                                   null, or the maxElementsInLeaf < 1.
+     */
+    public SimpleQuadTree(final List<T> elements, final int maxElementsInLeaf) throws IllegalArgumentException {
+        if (elements == null || maxElementsInLeaf < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.boundingBox = computeBoundingBox(elements);
+        this.maxLeafElements = maxElementsInLeaf;
+        createQuadTree(elements);
+
+    }
+
+    /**
+     * a constructor to create a SimpleQuadTree with 3 parameters. The bounding box
+     * is predefined.
+     *
+     * @param elements,          a list of elements to be saved in the quadtree.
+     * @param maxElementsInLeaf, the maximum limit of elements, in which a leaf node
+     *                           can save.
+     * @param boundingBox,       the smallest rectangle, in which all elements can
+     *                           be included.
+     * @throws IllegalArgumentException, if parameter is missing, or element ==
+     *                                   null, or the maxElementsInLeaf < 1.
+     */
+    private SimpleQuadTree(final List<T> elements, final int maxElementsInLeaf, final Rectangle boundingBox)
+            throws IllegalArgumentException {
+        if (elements == null || maxElementsInLeaf < 1) {
+            throw new IllegalArgumentException();
+        }
+        this.boundingBox = boundingBox;
+        this.maxLeafElements = maxElementsInLeaf;
+        createQuadTree(elements);
+    }
+
+    /**
+     * This returns a bounding box, which is smallest of all possible bounding boxes
+     * that include all elements of the list.
+     *
+     * @param elements, a list of elements to be saved in the quadtree, must not be
+     *                  null, must not contain null
+     * @return a bounding box in type Rectangle
+     */
+    private Rectangle computeBoundingBox(final List<T> elements) {
+        // TODO Insert code for assignment 5.1.b
+    	// we have to find the the most right down point of all elements.
+    	float X_max = 0 , Y_max = 0;
+    	float X_min = elements.get(0).getPosition().getXValue();
+    	float Y_min = elements.get(0).getPosition().getYValue();
+    	for( int i =0 ; i < elements.size(); i++ ) {
+    		Point current_point = elements.get(i).getPosition();
+    		float current_X = current_point.getXValue();
+    		float current_Y = current_point.getYValue();
+    		if (current_X > X_max) {X_max = current_X;}
+    		if (current_Y > Y_max) {Y_max = current_Y;}
+    		if (current_X > X_max) {X_max = current_X;}
+    		if (current_Y > Y_max) {Y_max = current_Y;}
+    	}
+    	//construct a new Rectangle, which is the bounding box
+    	Rectangle BoundingBox  = new Rectangle(X_min, Y_min, X_max-X_min, Y_max-Y_min);
+    	return BoundingBox;
+    }
+
+    /**
+     * This creates a quadtree using a recursive algorithm. If the size of the list is
+     * smaller than the maximum limit of leaf elements, a split is not necessary.
+     * All elements will be saved directly in the current node. Otherwise, a splitting
+     * into four child nodes is necessary. A linked list for each subnode will be
+     * initialized. The size of the bounding box of each subnode is the half of its
+     * width and height. This calls the Rectangle() function. Elements contained
+     * in the bounding box will be added into the linked list. With the list of
+     * elements and the bounding box as well as the maxLeafElements, it creates a
+     * SimpleQuadTree. This again includes the createQuadTree() function, which will
+     * be called recursively.
+     *
+     * @param list, a list of elements in type T
+     * @throws IllegalArgumentException, when parameter is missing.
+     */
+    void createQuadTree(final List<T> list) throws IllegalArgumentException {
+        // TODO Insert code for assignment 5.1.c
+    	Rectangle current_box = computeBoundingBox( list );
+    	float box_X = current_box.getX(), current_box_Y = current_box.getY();
+    	float box_Width = current_box.getWidth(), box_Height = current_box.getHeight();
+    	// these will store splitted element
+    	List<T> TL_Kind = new LinkedList<T>();
+    	List<T> TR_Kind = new LinkedList<T>();
+    	List<T> BL_Kind = new LinkedList<T>();
+    	List<T> BR_Kind = new LinkedList<T>();
+    	for ( int i = 0 ; i < list.size(); i++) {
+    		float point_X = list.get(i).getPosition().getXValue();
+    		float point_Y = list.get(i).getPosition().getYValue();
+    		if( point_X <= box_X + box_Width/2  &&  point_Y <= current_box_Y + box_Height/2) {
+    			TL_Kind.add(list.get(i));
+    		}
+    		else if( point_X > box_X + box_Width/2  &&  point_Y <= current_box_Y + box_Height/2) {
+    			TR_Kind.add(list.get(i));
+    		}
+    		else if( point_X <= box_X + box_Width/2  &&  point_Y > current_box_Y + box_Height/2) {
+    			BL_Kind.add(list.get(i));
+    		}
+    		else if( point_X > box_X + box_Width/2  &&  point_Y > current_box_Y + box_Height/2) {
+    			BR_Kind.add(list.get(i));
+    		}
+    	}
+    	
+    	if (TL_Kind.isEmpty()) { 
+    		this.topLeft = createSubTree(TL_Kind, computeBoundingBox(TL_Kind));
+    		createQuadTree( TL_Kind );
+    	}
+    	else if (!TL_Kind.isEmpty()) {this.topLeft = null;}
+    	
+    	if (TR_Kind.isEmpty()) {
+    		this.topRight = createSubTree(TR_Kind, computeBoundingBox(TR_Kind));
+    		createQuadTree( TR_Kind );
+    	}
+    	else if(!TR_Kind.isEmpty()) {this.topRight = null;}
+    	
+    	if (BL_Kind.isEmpty()) { 
+    		this.bottomLeft = createSubTree(BL_Kind, computeBoundingBox(BL_Kind));
+    		createQuadTree( BL_Kind);
+    	}
+    	else if(!BL_Kind.isEmpty()) {this.bottomLeft = null;}
+    	if (BR_Kind.isEmpty()) { 
+    		this.bottomRight = createSubTree(BR_Kind, computeBoundingBox(BR_Kind));
+    		createQuadTree( BR_Kind);
+    	}
+    	else if(!BR_Kind.isEmpty()) {this.bottomRight = null ;}
+    	
+    }
+
+    /**
+     * Creates a sub QuadTree with the elements contained in the bounding box.
+     * The QuadTree is empty if no elements are contained in the bounding box.
+     *
+     * @param allElements   all elements of the parent node
+     *                      (must be != null, must not contain null)
+     * @param boundingBox   the boundingBox to check if an element should be
+     *                      inserted into the sub QuadTree (must be != null)
+     * @return the SimpleQuadTree that contains all elements within the bounding box
+     */
+    private SimpleQuadTree<T> createSubTree(final List<T> allElements, final Rectangle boundingBox) {
+        assert allElements != null : "allElements is null";
+        assert !allElements.contains(null) : "allElements contains null";
+        assert boundingBox != null : "boundingBox is null";
+
+        final List<T> elements = new LinkedList<>();
+        for (final T item : allElements) {
+            if (boundingBox.containsPoint(item.getPosition())) {
+                elements.add(item);
+            }
+        }
+        return new SimpleQuadTree<>(elements, this.maxLeafElements, boundingBox);
+    }
+
+    /**
+     * This inserts all elements located in the requested range in resultList. It
+     * first checks whether the query intersects the whole quadtree. After that, the
+     * elements will be directly saved in resultList if this is a leaf node. Else it
+     * needs to go down to the subnodes recursively until the leaf node and then get
+     * the elements. The resultList only save the element, when it locates inside
+     * the requested range and is not already part of the list.
+     *
+     * @param resultList: List that is used to store the elements that are contained
+     *                    in the searching area, must be != null it is allowed that
+     *                    this list already contains elements, they are not removed,
+     *                    however this also does not make any sense
+     * @param query:      the searching area, must be != null
+     * @throws IllegalArgumentException if any parameter is invalid
+     */
+    @Override
+    public void rangeQuery(final List<T> resultList, final Rectangle query) {
+        // TODO Insert code for assignment 5.1.d
+    }
+}
